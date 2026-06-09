e5dfbb4c005dda2d06c53071aac9e61a965d150f
CaelanAccelerator <160204200+CaelanAccelerator@users.noreply.github.com>
2026-06-08 19:33:01 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+    m := make(map[int]int)
+    for i:= 0; i < len(nums); i++{
+        _, ok := m[target - nums[i]]
+        if ok {
+            return []int{m[target - nums[i]], i}
+        }
+        m[nums[i]] = i
+    }
+    return []int{}
+}
